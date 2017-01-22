ddd38d4d1213b353539916889cf6b99168d362ea
TheGlitch76 <theglitch76.dabug86@gmail.com>
2017-01-22 14:34:35 -0600

    Fixed my own mistakes
    while attempting to fix Spear's

--- lua/darkrp_customthings/jobs.lua
+++ lua/darkrp_customthings/jobs.lua
@@ -265,21 +265,21 @@ TEAM_SCAMMER = DarkRP.createJob("Indian Tech Support Scammer", {
 
 
 
----[[---------------------------------------------------------------------------
- -Define which team joining players spawn into and what team you change to if demoted
+--[[---------------------------------------------------------------------------
+ Define which team joining players spawn into and what team you change to if demoted
  ----------------------------------------------------------------------------]]
- -GAMEMODE.DefaultTeam = TEAM_CITIZEN
+ GAMEMODE.DefaultTeam = TEAM_CITIZEN
   
   
- ---[[---------------------------------------------------------------------------
- -Define which teams belong to civil protection
- -Civil protection can set warrants, make people wanted and do some other police related things
- ----------------------------------------------------------------------------]]
- -GAMEMODE.CivilProtection = {
- -	[TEAM_POLICE] = true,
- -	[TEAM_CHIEF] = true,
- -	[TEAM_MAYOR] = true,
- -}
+ --[[---------------------------------------------------------------------------
+ Define which teams belong to civil protection
+ Civil protection can set warrants, make people wanted and do some other police related things
+ ---------------------------------------------------------------------------]]
+ GAMEMODE.CivilProtection = {
+ 	[TEAM_POLICE] = true,
+ 	[TEAM_CHIEF] = true,
+ 	[TEAM_MAYOR] = true,
+ }
   
 
 --[[---------------------------------------------------------------------------
