526356d77a582cc8ef05eb0b7272d218855375cc
MrStrongSpear <thegameingtube@gmail.com>
2017-01-26 17:52:40 -0700

    Fixed error on Line 70
    Also removed a vape from the Drug-Dealer.

--- lua/darkrp_customthings/shipments.lua
+++ lua/darkrp_customthings/shipments.lua
@@ -67,55 +67,7 @@ DarkRP.createShipment("Medical Vape Shipment", {
 	--spawn = function(shipment_ent, shipment_data) print("Spawn custom entity and place it") end, -- Function to override how the shipment spawns the weapon when used.
 })
 
-DarkRP.createShipment(“Hallucinogenic Vape Single”, {
-	model = "models/swamponions/vape.mdl", -- The model of the item that hovers above the shipment
-	entity = "weapon_vape_hallucinogenic", -- the entity that comes out of the shipment
-	price = 10000, -- the price of one shipment
-	amount = 1, -- how many of the item go in one purchased shipment
-	separate = false, -- whether the item is sold separately (usually used for guns)
-	pricesep = 100, -- the price of a separately sold item
- 
-	-- The following fields are OPTIONAL. If you do not need them, or do not need to change them from their defaults, REMOVE them.
-	noship = false, -- whether this item has a shipment
-	allowed = {TEAM_DRUGDEALER}, -- OPTIONAL, which teams are allowed to buy this shipment/separate gun
-	shipmodel = "models/items/item_item_crate.mdl", -- OPTIONAL, the model of the shipment (this crate is the default)
-	weight = 30, -- OPTIONAL, the weight of the shipment. The default is the weight of the shipment 
-        buttonColor = Color(255, 255, 255, 255), -- Optional: The color of the button in the F4 menu,
-        label = “Hallucinogenic Vape Single”, -- Optional: the text on the button in the F4 menu
-        category = "Singles”, -- The name of the category it is in. Note: the category must be created!
-        sortOrder = 100, -- The position of this thing in its category. Lower number means higher up.
- 
-	-- Advanced, optional
-	shipmentClass = "spawned_shipment", -- The classname of the shipment entity. Use this if you have made a different shipment entity
-	onBought = function(ply, shipment, ent) end, -- function that is called when the shipment is bought
-	getPrice = function(ply, price) return ply:GetNWString("usergroup") == "donator" and price * 0.8 or price end, -- function to decide what the price is based on the player
-	--spawn = function(shipment_ent, shipment_data) print("Spawn custom entity and place it") end, -- Function to override how the shipment spawns the weapon when used.
-})
 
-DarkRP.createShipment(“Hallucinogenic Vape Shipment”, {
-	model = "models/swamponions/vape.mdl", -- The model of the item that hovers above the shipment
-	entity = "weapon_vape_hallucinogenic", -- the entity that comes out of the shipment
-	price = 10000, -- the price of one shipment
-	amount = 1, -- how many of the item go in one purchased shipment
-	separate = false, -- whether the item is sold separately (usually used for guns)
-	pricesep = 100, -- the price of a separately sold item
- 
-	-- The following fields are OPTIONAL. If you do not need them, or do not need to change them from their defaults, REMOVE them.
-	noship = false, -- whether this item has a shipment
-	allowed = {TEAM_DRUGDEALER}, -- OPTIONAL, which teams are allowed to buy this shipment/separate gun
-	shipmodel = "models/items/item_item_crate.mdl", -- OPTIONAL, the model of the shipment (this crate is the default)
-	weight = 30, -- OPTIONAL, the weight of the shipment. The default is the weight of the shipment 
-        buttonColor = Color(255, 255, 255, 255), -- Optional: The color of the button in the F4 menu,
-        label = “Hallucinogenic Vape Single”, -- Optional: the text on the button in the F4 menu
-        category = "Singles”, -- The name of the category it is in. Note: the category must be created!
-        sortOrder = 100, -- The position of this thing in its category. Lower number means higher up.
- 
-	-- Advanced, optional
-	shipmentClass = "spawned_shipment", -- The classname of the shipment entity. Use this if you have made a different shipment entity
-	onBought = function(ply, shipment, ent) end, -- function that is called when the shipment is bought
-	getPrice = function(ply, price) return ply:GetNWString("usergroup") == "donator" and price * 0.8 or price end, -- function to decide what the price is based on the player
-	--spawn = function(shipment_ent, shipment_data) print("Spawn custom entity and place it") end, -- Function to override how the shipment spawns the weapon when used.
-})
 
 DarkRP.createShipment(“Weed Single”, {
 	model = "durgz_weed", -- The model of the item that hovers above the shipment
